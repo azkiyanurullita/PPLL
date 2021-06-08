80f5a6d288d16fb2ac216993c6616d2f4d68c98a
eka putri <62703120+students222@users.noreply.github.com>
2021-06-08 20:37:42 +0700

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,16 @@
+import pm_lexer
+import pm_parser
+import pm_interpreter
+
+from sys import *
+
+#DENGAN MASUKAN .px
+lexer = pm_lexer.BasicLexer()
+parser = pm_parser.BasicParser()
+env = {}
+
+file = open(argv[1])
+text = file.readlines()
+for line in text:
+    tree = parser.parse(lexer.tokenize(line))
+    pm_interpreter.BasicExecute(tree, env)
