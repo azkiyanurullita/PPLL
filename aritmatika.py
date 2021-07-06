4b3891658453122a5c6c65451e5cb7859693a1ba
kireina <68951823+KireinaYukiPratiwi@users.noreply.github.com>
2021-07-06 23:24:56 +0700

    Add files via upload

---
+++ aritmatika.py
@@ -0,0 +1,32 @@
+# Contoh operasi aritmatika
+
+# Penjumlahan
+4+2
+
+# Pengurangan
+4-2
+
+# Perkalian
+4*2
+
+# Pembagian
+4/2
+
+CETAK "Bisa juga dengan cara seperti dibawah ini"
+
+# Mendeklarasikan isi variabel terlebih dahulu baru 
+# kemudian variabelnya dioperasikan
+a=4
+b=2
+
+# Penjumlahan
+CETAK a+b
+
+# Pengurangan
+CETAK a-b
+
+# Perkalian
+CETAK a*b
+
+# Pembagian
+CETAK a/b
---
+++ fungsi.py
@@ -0,0 +1,5 @@
+# Contoh pembuatan fungsi
+
+FUNGSI coba() -> CETAK "Buwung apa tu man?"
+
+coba()
